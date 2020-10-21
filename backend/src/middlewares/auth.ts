5cfd674f6787c01dd7a9abe9f13c415de1fc6e0e
Lais <lays.rhianne@gmail.com>
2020-10-21 14:48:06 -0300

    auth updated

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -1,13 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-module.exports = (req: Request, res: Response, next: NextFunction) => {
+export default (req: Request, res: Response, next: NextFunction) => {
     try {
         let token = '';
         if (req.headers.access_token){
         token = (<string>req.headers.access_token).split(' ')[1];
         }
-        jwt.verify(token, process.env.SECRET_KEY);
+        jwt.verify(token, <string>process.env.SECRET_KEY);
         next();
     } catch (error) {
         res.status(401).json({ message: "Authentication failed!" });
