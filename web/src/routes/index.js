52c708419d85c359651c98398463f3c28521dd1e
Laís Rhianne <lays.rhianne@gmail.com>
2020-11-05 14:01:31 -0300

    routes fixed

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,12 +3,12 @@ import { Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import MyRoute from './MyRoute';
-import Landing from '../pages/Landing';
+/* import Landing from '../pages/Landing';
 import Cadastro from '../pages/Cadastro';
 import Login from '../pages/Login';
-import Calculo from '../pages/Calculo';
 import CalculoLogado from '../pages/CalculoLogado';
-import Produtos from '../pages/Produtos';
+import Produtos from '../pages/Produtos'; */
+import Calculo from '../pages/Calculo';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
